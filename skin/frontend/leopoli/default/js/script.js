ddca70723f4e78adc5c2572b7b8bf41e6daf5be1
Roman <romanpanchyshyn@gmail.com>
2014-05-09 18:15:20 +0300

    many changes

--- skin/frontend/leopoli/default/js/script.js
+++ skin/frontend/leopoli/default/js/script.js
@@ -1,5 +1,26 @@
 jQuery.noConflict();
 jQuery( document ).ready(function(){
+
+    //Zoom Plugin
     jQuery('.zoom').zoom();
+
+    //Count inc dec
+    jQuery('.count span').click(function(){
+        var input = jQuery(this).parent().find('input');
+        var dec = jQuery(this).parent().find('.dec');
+        var inputVal = input.val();
+        if(jQuery(this).hasClass('inc')) {
+           inputVal++;
+           dec.removeClass('no-Active');
+        }
+        if(jQuery(this).hasClass('dec')) {
+            inputVal--;
+            if(inputVal < 2) {
+                dec.addClass('no-Active');
+                inputVal = 1;
+            }
+        }
+        input.val(inputVal);
+    });
 });	
   
